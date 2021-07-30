de32ca5e7c78cd40719a707fc842081f639086e2
hkancherla13 <87538438+hkancherla13@users.noreply.github.com>
2021-07-29 23:00:17 -0500

    final commit

--- cypress/integration/crud_test.js
+++ cypress/integration/crud_test.js
@@ -4,37 +4,49 @@
             cy.visit('http://localhost:3000/')
             cy.title().should('eq','Productotron 3000')
         });
-       
+       //Creating a record
       it("Add a record", () => {
-        cy.xpath('//*[@id="root"]/section/section/aside/div/ul/li/span[2]').click()
-        
-        cy.xpath('/html/body/div[2]/div/div[2]/div')
-       
-        
-        cy.get('#name').type('Test_Product')
-        
-        cy.get('#description').type('xyz')
-        cy.get('#price').type('100')
-        cy.get('#inventory').type('5')
-        cy.xpath('//button[@class="ant-btn ant-btn-primary"]/span[text()="Create"]').click()
-        cy.xpath('/html/body/div[3]/div/div/div/div/div[@class="ant-message-custom-content ant-message-success"]').should('exist')
+        cy.xpath('//*[@id="root"]/section/section/aside/div/ul/li/span[2]').click() //Clicking on Create Product
+       cy.get('#name').type(productname(5)) //Entering the Name
+        cy.get('#description').type('xyz') //Entering the product description
+        cy.get('#price').type('100') //Entering the Price
+        cy.get('#inventory').type('5') //Entering the Inventory
+        cy.xpath('//button[@class="ant-btn ant-btn-primary"]/span[text()="Create"]').click() // Clicking on Create button
+        cy.xpath('/html/body/div[3]/div/div/div/div/div[@class="ant-message-custom-content ant-message-success"]').should('exist') //verifying if the toast message content is visible
       });
 
+      //Displaying all the records
+      it("Display a record", () => {
+        cy.get('#root > section > section > main > div').children().should('exist')
+      });
+
+     //Updating a record
       it("Update a record", () => {
-      
-        cy.xpath('//*[@id="root"]/section/section/main/div/div[7]/div/ul/li[1]/span/span').click()
-        cy.get('#name').clear().type('Updated Test Product ')
-        cy.get('body > div:nth-child(2) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-footer > button.ant-btn.ant-btn-primary').click()
- 
+        cy.xpath('//*[@id="root"]/section/section/main/div/div[7]/div/ul/li[1]/span/span').click() //Clicking on Edit 
+        cy.get('#name').clear().type('Updated Test Product ') // Updating the Product Name
+        cy.get('body > div:nth-child(2) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-footer > button.ant-btn.ant-btn-primary').click() //Saving the edited changes
        });
 
+       //Deleting a record
       it("Delete a record", () => {
-      
-        cy.get('#root > section > section > main > div > div:nth-child(7) > div > ul > li:nth-child(2) > span > span').click()
-        cy.xpath('/html/body/div[2]/div/div/div/div[2]/div/div[2]/button[2]').click()
-        cy.xpath('/html/body/div[2]/div/div/div/div/div').should('exist') 
-           
- 
-       }); 
+        cy.get('#root > section > section > main > div > div:nth-child(7) > div > ul > li:nth-child(2) > span > span').click() //Clicking on Delete
+        cy.xpath('/html/body/div[2]/div/div/div/div[2]/div/div[2]/button[2]').click() // Clicking on Yes 
+        cy.xpath('/html/body/div[2]/div/div/div/div/div').should('exist') //verifying if the toast message content is visible
+       });  
   
     });  
+
+
+    function productname(length) {
+      var name           = '';
+      var characters       = 'abcdefghijklmnopqrstuvwxyz';
+      var charactersLength = characters.length;
+      for ( var i = 0; i < length; i++ ) {
+        name += characters.charAt(Math.floor(Math.random() * 
+   charactersLength));
+     }
+     return name;
+  }
+    
+
+   
